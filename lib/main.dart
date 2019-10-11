b9452edceb09ab98893248ec4f6295690f79f58e
Varun Resh Maddali <mvresh@gmail.com>
2019-10-11 17:20:30 +0530

    I completed the exercise but some unused or repeated code is present. I'm keeping it so that I could use it in another approach I'm using.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,13 @@ class TicTacToePage extends StatefulWidget {
   _TicTacToePageState createState() => _TicTacToePageState();
 }
 
+List<List<Widget>> BoardOfWidgets = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+String currentPlayer = 'X';
+
 class _TicTacToePageState extends State<TicTacToePage> {
   @override
   Widget build(BuildContext context) {
@@ -25,29 +32,222 @@ class _TicTacToePageState extends State<TicTacToePage> {
           ),
           Column(
             children: <Widget>[
-              SizedBox(height: 100,),
-              Text('TIC TAC TOE', style: TextStyle(
-                fontFamily:'Varela',
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-                color: Colors.white
-              )),
-              Text('Player X to move', style: TextStyle(
-                fontFamily:'Varela',
-                fontSize: 20,
-                  color: Colors.white70
-              )),
-              ReusableRow(),
-              ReusableRow(),
-              ReusableRow(),
-              SizedBox(height: 30,),
-              ReusableBox(width:160,height: 60,child: Center(
-                child: Text('Result', style: TextStyle(
-                    fontFamily:'Varela',
-                    fontSize: 24,
-                    color: Color(0xFFFFFFFF),
-                )),
-              ),),
+              SizedBox(
+                height: 100,
+              ),
+              Text('TIC TAC TOE',
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Colors.white)),
+              Text('Player ${currentPlayer} to move',
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 20,
+                      color: Colors.white70)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableBox(
+                      width: 70,
+                      height: 70,
+                      rowNumber: 1,
+                      itemNumber: 1,
+                      child: BoardOfWidgets[0][0],
+                      createText: () {
+                        setState(() {
+                          BoardOfWidgets[0][0] = Text(
+                            currentPlayer,
+                            style: TextStyle(fontSize: 35, color: Colors.white),
+                          );
+                          currentPlayer == 'X'
+                              ? currentPlayer = 'O'
+                              : currentPlayer = 'X';
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 1,
+                        itemNumber: 2,
+                        child: BoardOfWidgets[0][1],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[0][1] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 1,
+                        itemNumber: 3,
+                        child: BoardOfWidgets[0][2],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[0][2] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 2,
+                        itemNumber: 1,
+                        child: BoardOfWidgets[1][0],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[1][0] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 2,
+                        itemNumber: 2,
+                        child: BoardOfWidgets[1][1],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[1][1] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 2,
+                        itemNumber: 3,
+                        child: BoardOfWidgets[1][2],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[1][2] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 3,
+                        itemNumber: 1,
+                        child: BoardOfWidgets[2][0],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[2][0] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 3,
+                        itemNumber: 2,
+                        child: BoardOfWidgets[2][1],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[2][1] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                  Expanded(
+                    child: ReusableBox(
+                        width: 70,
+                        height: 70,
+                        rowNumber: 3,
+                        itemNumber: 3,
+                        child: BoardOfWidgets[2][2],
+                        createText: () {
+                          setState(() {
+                            BoardOfWidgets[2][2] = Text(
+                              currentPlayer,
+                              style: TextStyle(fontSize: 35, color: Colors.white),
+                            );
+                            currentPlayer == 'X'
+                                ? currentPlayer = 'O'
+                                : currentPlayer = 'X';
+                          });
+                        }),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ReusableBox(
+                width: 160,
+                height: 60,
+                child: Center(
+                  child: Text('Result',
+                      style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 24,
+                        color: Color(0xFFFFFFFF),
+                      )),
+                ),
+              ),
             ],
           )
         ],
@@ -57,25 +257,25 @@ class _TicTacToePageState extends State<TicTacToePage> {
 }
 
 class ReusableRow extends StatelessWidget {
-  const ReusableRow({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Expanded(
-          child: ReusableBox(width: 70,height: 70),
+          child: ReusableBox(
+            width: 70,
+            height: 70,
+            itemNumber: 1,
+            child: null,
+          ),
         ),
         Expanded(
-          child: ReusableBox(width: 70,height: 70),
+          child: ReusableBox(width: 70, height: 70, itemNumber: 2, child: null),
         ),
         Expanded(
-          child: ReusableBox(width: 70,height: 70),
+          child: ReusableBox(width: 70, height: 70, itemNumber: 3, child: null),
         ),
-
       ],
     );
   }
@@ -85,17 +285,30 @@ class ReusableBox extends StatelessWidget {
   double width;
   double height;
   Widget child;
-  ReusableBox({this.width ,this.height,this.child});
+  int itemNumber;
+  int rowNumber;
+  Function createText;
+
+  ReusableBox(
+      {this.width,
+      this.height,
+      this.child,
+      this.itemNumber,
+      this.rowNumber,
+      this.createText});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,height: height,
-      margin: EdgeInsets.all(12),
-      child: child,
-      decoration: BoxDecoration(
-        color: Color(0xFFFFFFFF).withOpacity(0.4),
-        borderRadius: BorderRadius.all(Radius.circular(12))
+    return GestureDetector(
+      onTap: createText,
+      child: Container(
+        width: width,
+        height: height,
+        margin: EdgeInsets.all(12),
+        child: Center(child: child),
+        decoration: BoxDecoration(
+            color: Color(0xFFFFFFFF).withOpacity(0.4),
+            borderRadius: BorderRadius.all(Radius.circular(12))),
       ),
     );
   }
